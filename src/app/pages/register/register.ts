0a4a25e93ceff41e5fcec203eaa84cba390805f2
Thiago Castro <nascimentocastro989@gmail.com>
2026-06-13 23:05:54 -0300

    Ocultador de senha adicionado

--- src/app/pages/register/register.ts
+++ src/app/pages/register/register.ts
@@ -5,10 +5,11 @@ import {MatSelectModule} from '@angular/material/select';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatIconModule} from '@angular/material/icon';
+import { PasswordField } from '../../shared/components/password-field/password-field';
 
 @Component({
   selector: 'app-register',
-  imports: [MatCardModule, MatButtonModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatIconModule],
+  imports: [MatCardModule, MatButtonModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatIconModule, PasswordField],
   templateUrl: './register.html',
   styleUrl: './register.scss',
   encapsulation: ViewEncapsulation.None
